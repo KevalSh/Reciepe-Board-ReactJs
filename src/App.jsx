e8447e1f9f64b201aa87e11d746272965aa078ca
Keval Shah <keval1916@gmail.com>
2025-04-04 16:28:07 +0530

    Added the styling using tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -16,41 +16,49 @@ export default function App() {
   const [expenseList, setExpenseList] = useState([]);
 
   const creditEntry = () => {
-    const creditExpenseList = [...expenseList];
+    if (inputValue !== "") {
+      const creditExpenseList = [...expenseList];
 
-    const transactions = {
-      id: Math.random(),
-      type: "Credit",
-      amount: inputValue,
-    };
+      const transactions = {
+        id: Math.random(),
+        type: "Credit",
+        amount: inputValue,
+      };
 
-    creditExpenseList.push(transactions);
+      creditExpenseList.push(transactions);
 
-    const creditor = Number(balance) + Number(inputValue);
+      const creditor = Number(balance) + Number(inputValue);
 
-    //console.log(creditExpenseList, creditor);
+      //console.log(creditExpenseList, creditor);
 
-    setBalance(creditor);
-    setInputValue("");
-    setExpenseList(creditExpenseList);
+      setBalance(creditor);
+      setInputValue("");
+      setExpenseList(creditExpenseList);
+    } else {
+      alert("Please enter amount");
+    }
   };
 
   const debitEntry = () => {
-    const debitExpenseList = [...expenseList];
-    // structure of the transaction object
-    const transactions = {
-      id: Math.random(),
-      type: "Dedit",
-      amount: inputValue,
-    };
+    if (inputValue !== "") {
+      const debitExpenseList = [...expenseList];
+      // structure of the transaction object
+      const transactions = {
+        id: Math.random(),
+        type: "Dedit",
+        amount: inputValue,
+      };
 
-    debitExpenseList.push(transactions);
+      debitExpenseList.push(transactions);
 
-    const debitor = balance - Number(inputValue);
+      const debitor = balance - Number(inputValue);
 
-    setBalance(debitor);
-    setInputValue("");
-    setExpenseList(debitExpenseList);
+      setBalance(debitor);
+      setInputValue("");
+      setExpenseList(debitExpenseList);
+    } else {
+      alert("please enter amount");
+    }
   };
 
   const removeExpense = (index, amount) => {
@@ -70,22 +78,48 @@ export default function App() {
 
   return (
     <div>
-      <h3> Total Balance : {balance} </h3>
+      <h1 class="m-2 p-2 text-teal-500 bg-stone-100 text-center text-2xl rounded-2xl font-extrabold">
+        Expense Calculator
+      </h1>
+      <h3 class="p-4 mt-8 bg-slate-500 text-center border-3 border-indigo-300 text-amber-100 text-xl font-bold">
+        {" "}
+        Total Balance : {balance}{" "}
+      </h3>
       <input
         type="text"
         placeholder="Add the amount"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
+        class="m-8 p-1 bg-zinc-500 border-2 border-indigo-300 rounded-md text-white font-bold "
       />
-      <button onClick={creditEntry}>Credit Entry</button>
-      <button className="bg-red-50" onClick={debitEntry}>
+      <button
+        class="p-2 m-2 bg-green-600 text-white rounded-md font-medium hover:bg-green-700"
+        onClick={creditEntry}
+      >
+        Credit Entry
+      </button>
+      <button
+        class="p-2 m-2 bg-red-600 text-white rounded-md font-medium hover:bg-red-700"
+        onClick={debitEntry}
+      >
         Debit Entry
       </button>
       <ul>
         {expenseList.map((amount, index) => (
-          <div key={index}>
-            {amount.type} -- {amount.amount}
-            <button onClick={() => removeExpense(index, amount)}>Remove</button>
+          <div
+            class="m-2 flex justify-around bg-slate-500 rounded-md text-white font-medium"
+            key={index}
+          >
+            <div class="p-4">
+              {amount.type} -- {amount.amount}
+            </div>
+
+            <button
+              class="p-2 m-2 bg-yellow-500 text-white rounded-md font-medium hover:bg-slate-800 hover:text-yellow-500"
+              onClick={() => removeExpense(index, amount)}
+            >
+              Remove
+            </button>
           </div>
         ))}
       </ul>
